9b3d9d0dbce31386a41f119e8500a15db3f63631
Matthew Condit <matthew@humanyze.com>
2018-03-13 15:27:03 -0400

    fix placement of arg

--- scripts/test.js
+++ scripts/test.js
@@ -21,8 +21,10 @@ const argv = process.argv.slice(2);
 // Watch unless on CI or in coverage mode
 if (!process.env.CI && argv.indexOf('--coverage') < 0) {
   argv.push('--watch');
-  argv.push('--showConfig')
+} else {
+    argv.push('--showConfig');
 }
+
 
 
 jest.run(argv);
