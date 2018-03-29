2a702a3bc78ba95756803574e83c5fda6cdeb773
Matthew Condit <matthew@humanyze.com>
2018-03-29 12:13:44 -0400

    remove class usage for modal components for consistency

--- src/components/modal/model-components/export-equipment-data/ExportEquipmentDataModal.js
+++ src/components/modal/model-components/export-equipment-data/ExportEquipmentDataModal.js
@@ -1,20 +1,15 @@
 import React, { Component } from 'react';
 import LightBoxWrapper from '../LightBoxWrapper/LightBoxWrapper';
 
-class ExportEquipmentDataModal extends Component {
-    state = {};
-
-
-    render() {
-        return (
-            <LightBoxWrapper>
-                <div className='ExportEquipmentData'>
-                    Hello World ExportEquipmentDataModal
-                    <button onClick={this.props.closeModal}>Close</button>
-                </div>
-            </LightBoxWrapper>
-        );
-    }
+const ExportEquipmentDataModal = () => {
+    return (
+        <LightBoxWrapper>
+            <div className='ExportEquipmentData'>
+                Hello World ExportEquipmentDataModal
+                <button onClick={this.props.closeModal}>Close</button>
+            </div>
+        </LightBoxWrapper>
+    );
 };
 
 export default ExportEquipmentDataModal;
--- src/components/modal/model-components/import-participant-data/ImportParticipantDataModal.js
+++ src/components/modal/model-components/import-participant-data/ImportParticipantDataModal.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
 import LightBoxWrapper from '../LightBoxWrapper/LightBoxWrapper';
 
-class ImportEquipmentDataModal extends Component {
-    state = {};
-
-
-    render() {
-        return (
-            <LightBoxWrapper>
-                <div className='ImportEquipmentData'>
-                    <div>Hello World ImportEquipmentDataModal</div>
-                    <div>
-                        <button onClick={this.props.closeModal}>Close</button>
-                    </div>
+const ImportEquipmentDataModal = () => {
+    return (
+        <LightBoxWrapper>
+            <div className='ImportEquipmentData'>
+                <div>Hello World ImportEquipmentDataModal</div>
+                <div>
+                    <button onClick={this.props.closeModal}>Close</button>
                 </div>
-            </LightBoxWrapper>
-        );
-    }
-}
+            </div>
+        </LightBoxWrapper>
+    );
+};
 
 export default ImportEquipmentDataModal;
