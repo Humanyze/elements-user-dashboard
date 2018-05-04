ee56a82a67cf332f6cbd97b96ecdfbec1147797c
Matthew Condit <matthew@humanyze.com>
2018-05-04 15:47:01 -0400

    add material styles, fix onclickelsewhere bug

--- src/components/common/date-selector/dateSelector.js
+++ src/components/common/date-selector/dateSelector.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import Moment from 'moment';
 import MaterialIcon from 'material-icons-react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-// import { compose, withHandlers, withState } from 'recompose';
 
 
 import { getCurrentTranslations } from 'Src/redux/language/languageReducer';
@@ -13,32 +11,52 @@ import './date-selector.scss';
 
 // must be class for ref usage by DatePicker
 class CustomInput extends React.Component {
+
+    handleClick = (e) => {
+        const { container } = this.props;
+        const open = container.datepicker.state.open;
+        open ? container.datepicker.setOpen(false) : this.props.onClick();
+    };
+
     render() {
-        const { translations, value, onClick } = this.props;
+        const { translations, value } = this.props;
 
         return (
-            <div onClick={onClick} className='DateSelector__label-button'>
-                {/* NOTE: Don't like dependency on Moment-js here */}
-                {value ?
-                    <div className='DateSelector__date-text'>{Moment(value._d).format('MMM D, YYYY')}</div> :
-                    <div className='DateSelector__select-date-text'>{translations['DateSelector__select-date']}</div>}
+            <div onClick={this.handleClick} className='DateSelector__label-button react-datepicker-ignore-onclickoutside'>
+
+                {!!value ?
+                    <div className='DateSelector__date-text'>{value}</div> :
+                    <div className='DateSelector__select-date-text'>{translations['DateSelector__select-date']}</div>
+                }
+
                 <MaterialIcon icon='event_note' size={18}/>
             </div>
         );
     }
 }
 
-export const DateSelectorPure = ({ translations, date, onChange }) => {
-    return (
-        <div className='DateSelector'>
-            <div className='DateSelector__dropdown'>
-                <DatePicker onSelect={onChange}
-                            selected={date}
-                            customInput={<CustomInput translations={translations} value={date}/>}/>
+export class DateSelectorPure extends React.Component {
+    constructor() {
+        super();
+        this.datepicker = { state: { open: false } };
+    }
+
+    render() {
+        const { translations, date, onChange } = this.props;
+        return (
+            <div className='DateSelector'>
+                <div className='DateSelector__dropdown'>
+                    <DatePicker onSelect={onChange}
+                                selected={date}
+                                dateFormat={'MMM D, YYYY'}
+                                useWeekdaysShort={true}
+                                ref={datepicker => this.datepicker = datepicker}
+                                customInput={<CustomInput translations={translations} container={this}/>}/>
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    };
+}
 
 
 const DateSelector = connect(
