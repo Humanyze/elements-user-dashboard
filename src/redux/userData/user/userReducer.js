a7ebf8406fb1db44ffb6c1041db48a68b412ceaa
Matthew Condit <matthew@humanyze.com>
2018-05-21 14:34:30 -0400

    add restriction for routes in avatar menu

--- src/redux/userData/user/userReducer.js
+++ src/redux/userData/user/userReducer.js
@@ -1,36 +1,48 @@
 import { handleActions } from 'redux-actions';
 import USER_ACTION_TYPES from './userActionTypes';
 import AUTH_ACTION_TYPES from 'Redux/auth/authActionTypes';
+import { createSelector } from 'reselect';
 
 
 const getCurrentUser = (state) => state.user.user.user;
-const getCurrentUserName = (state) => getCurrentUser(state) && getCurrentUser(state).username;
+
+const getCurrentUserName = createSelector(
+    getCurrentUser,
+    (currentUser) => currentUser && currentUser.username
+);
+
+const getCurrentUserPermissions = createSelector(
+    getCurrentUser,
+    (currentUser) => (currentUser && currentUser.features) || {}
+);
+
 
 export {
     getCurrentUser,
-    getCurrentUserName
+    getCurrentUserName,
+    getCurrentUserPermissions
 };
 
 export const initialState = {
     requestPending: false,
-    user: null
+    user          : null
 };
 
 const userReducer = handleActions({
-    [USER_ACTION_TYPES.USER_DATA_FETCH_REQUESTED]: (state, action) => ({
+    [USER_ACTION_TYPES.USER_DATA_FETCH_REQUESTED] : (state, action) => ({
         ...state,
         requestPending: true
     }),
     [USER_ACTION_TYPES.USER_DATA_FETCH_SUCCESSFUL]: (state, action) => ({
         ...state,
-        user: action.payload,
+        user          : action.payload,
         requestPending: false
     }),
-    [USER_ACTION_TYPES.USER_DATA_FETCH_FAILED]: (state, action) => ({
+    [USER_ACTION_TYPES.USER_DATA_FETCH_FAILED]    : (state, action) => ({
         ...state,
         requestPending: false
     }),
-    [AUTH_ACTION_TYPES.LOGOUT]: (state, action) => initialState
+    [AUTH_ACTION_TYPES.LOGOUT]                    : (state, action) => initialState
 }, initialState);
 
 
