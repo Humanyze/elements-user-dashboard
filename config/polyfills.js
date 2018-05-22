c33d9a97cddd4a56937938d7e4d7b9214bd4ba1d
Matthew Condit <matthew@humanyze.com>
2018-05-22 11:20:10 -0400

    add ie polyfill for string includes

--- config/polyfills.js
+++ config/polyfills.js
@@ -20,3 +20,18 @@ Object.assign = require('object-assign');
 if (process.env.NODE_ENV === 'test') {
   require('raf').polyfill(global);
 }
+
+if (!String.prototype.includes) {
+    String.prototype.includes = function(search, start) {
+        'use strict';
+        if (typeof start !== 'number') {
+            start = 0;
+        }
+
+        if (start + search.length > this.length) {
+            return false;
+        } else {
+            return this.indexOf(search, start) !== -1;
+        }
+    };
+}
