77bb7604c96faba8860408b33ed66b5a01c6a6e3
Matthew Condit <matthew@humanyze.com>
2018-05-29 14:13:46 -0400

    change circleci to chose respective ember bucket

--- src/redux/error/errorMessageTypes.js
+++ src/redux/error/errorMessageTypes.js
@@ -1,6 +1,6 @@
 const PRIORITIES = {
-    LOW: 1,
-    MED: 2,
+    LOW : 1,
+    MED : 2,
     HIGH: 3
 };
 
@@ -12,57 +12,67 @@ export const MESSAGE_TYPES = {
 
 const participantExportFailure = {
     messageTranslationKey: 'errorMessage__participantExportFailure',
-    type: MESSAGE_TYPES.FLASH,
-    priority: PRIORITIES.LOW
+    type                 : MESSAGE_TYPES.FLASH,
+    priority             : PRIORITIES.LOW
 };
 
 const userFetchFailure = {
     messageTranslationKey: 'errorMessage__userFetchFailure',
-    type: MESSAGE_TYPES.FATAL,
-    priority: PRIORITIES.HIGH,
-    redirectButtons: [{
-        link: '/logout',
-        textKey: 'errorMessage__userFetchFailure--button'
-    }]
+    type                 : MESSAGE_TYPES.FATAL,
+    priority             : PRIORITIES.HIGH,
+    redirectButtons      : [
+        {
+            link   : '/logout',
+            textKey: 'errorMessage__userFetchFailure--button'
+        }
+    ]
 };
 
 const userUnauthorizedFailure = {
     messageTranslationKey: 'errorMessage__userUnauthorizedFailure',
-    type: MESSAGE_TYPES.FATAL,
-    priority: PRIORITIES.MED,
-    redirectButtons: [{
-        link: '/deployments/select-deployment',
-        textKey: 'errorMessage__userUnauthorizedFailure--button',
-        reload: true
-    }]
+    type                 : MESSAGE_TYPES.FATAL,
+    priority             : PRIORITIES.MED,
+    redirectButtons      : [
+        {
+            link   : '/deployments/select-deployment',
+            textKey: 'errorMessage__userUnauthorizedFailure--button',
+            reload : true
+        }
+    ]
 };
 
 const genericDeploymentFailure = {
     messageTranslationKey: 'errorMessage__genericDeploymentFailure',
-    type: MESSAGE_TYPES.FATAL,
-    priority: PRIORITIES.MED,
-    redirectButtons: [{
-        link: '', // forces reload
-        textKey: 'errorMessage__genericDeploymentFailure--button',
-        reload: true
-    }, {
-        link: '/logout',
-        textKey: 'logout',
-    }]
+    type                 : MESSAGE_TYPES.FATAL,
+    priority             : PRIORITIES.MED,
+    redirectButtons      : [
+        {
+            link   : '', // forces reload
+            textKey: 'errorMessage__genericDeploymentFailure--button',
+            reload : true
+        },
+        {
+            link   : '/logout',
+            textKey: 'logout',
+        }
+    ]
 };
 
 const deploymentFetchFailure = {
     messageTranslationKey: 'errorMessage__deploymentFetchFailure',
-    type: MESSAGE_TYPES.FATAL,
-    priority: PRIORITIES.MED,
-    redirectButtons: [{
-        link: '', // forces reload
-        textKey: 'errorMessage__deploymentFetchFailure--button',
-        reload: true
-    }, {
-        link: '/logout',
-        textKey: 'logout',
-    }]
+    type                 : MESSAGE_TYPES.FATAL,
+    priority             : PRIORITIES.MED,
+    redirectButtons      : [
+        {
+            link   : '', // forces reload
+            textKey: 'errorMessage__deploymentFetchFailure--button',
+            reload : true
+        },
+        {
+            link   : '/logout',
+            textKey: 'logout',
+        }
+    ]
 };
 
 export default {
