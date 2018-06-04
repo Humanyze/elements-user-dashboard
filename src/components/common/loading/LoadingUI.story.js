2131348cb17a1c572c73bc96be1bbec62158354d
Matthew Condit <matthew@humanyze.com>
2018-06-04 15:13:17 -0400

    move common comps to git submodule

--- src/components/common/loading/LoadingUI.story.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import  LoadingUI from './LoadingUI';
-
-const createComp = (props) => <LoadingUI {...props}/>;
-
-storiesOf('Loading UI', module)
-    .add('initial', () => createComp());
