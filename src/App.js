a70006f55d8cef5afadda1fa9770cd24bde1e8b5
elbeezy <djlb1108@gmail.com>
2018-10-11 15:30:23 -0400

    add GA tracking

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import AppRoutes from './components/AppRoutes';
 import ModalRoot from './components/modal/ModalRoot';
 import RouterPaths from 'RouterPaths';
 
+import ReactGA from 'react-ga';
+ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const App = () => (
     <div>
