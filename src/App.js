6d41afa80ddde1010f9ff8c10ce60b6ef5806f48
elbeezy <djlb1108@gmail.com>
2018-10-12 10:19:34 -0400

    add & use react-with-analytics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-
+import {
+  BrowserRouter as Router,
+  withRouter
+} from 'react-router-dom';
+import { compose } from 'recompose';
+import withAnalytics, { initAnalytics } from 'react-with-analytics';
 import Header from 'Common/header/Header';
 import AppRoutes from './components/AppRoutes';
 import ModalRoot from './components/modal/ModalRoot';
 import RouterPaths from 'RouterPaths';
 
-import ReactGA from 'react-ga';
-ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
-ReactGA.pageview(window.location.pathname + window.location.search);
+
+initAnalytics(process.env.REACT_APP_GA_TRACKING_ID);
+
+const enhance = compose(
+  withRouter,
+  withAnalytics
+);
+
+const CorePure = () => (
+  <div>
+    <Header/>
+    <AppRoutes/>
+    <ModalRoot/>
+  </div>
+);
+
+const Core = enhance(CorePure);
 
 const App = () => (
-    <div>
-        <Router basename={RouterPaths.basePath}>
-                <div>
-                    <Header/>
-                    <AppRoutes/>
-                    <ModalRoot/>
-                </div>
-        </Router>
-    </div>
+  <div>
+    <Router basename={RouterPaths.basePath}>
+      <Core />
+    </Router>
+  </div>
 );
 
 export default App;
