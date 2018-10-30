16cd4e056780785d30d0ce076b4bf40f5a5d45d6
elbeezy <djlb1108@gmail.com>
2018-10-30 15:48:54 -0400

    add unit-type data filter to adjacencies

--- src/components/digital/dashboards/digital-filter-routes/FilterRoutes.js
+++ src/components/digital/dashboards/digital-filter-routes/FilterRoutes.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { Fragment } from 'react';
+import { Route, Switch, withRouter } from 'react-router-dom';
 import RouterPaths from 'Src/routerPaths';
 import { METRIC_FILTER_KEYS } from 'Src/redux/common/filter-ui/filterUIReducer';
 import FilterComposeHOF from 'Src/components/common/metric-filter-block/FilterComposeHOF';
@@ -7,7 +7,7 @@ import DropdownSelector from 'Src/components/common/dropdown-selector/DropdownSe
 import { compose } from 'recompose';
 
 
-const createFilterComponent = (config) => compose(FilterComposeHOF(config))(DropdownSelector);
+const createFilterComponent = (config) => compose(withRouter, FilterComposeHOF(config))(DropdownSelector);
 
 const filterConfigs = {
   adjacenciesComparison: {
@@ -18,17 +18,30 @@ const filterConfigs = {
     ],
     filterKey      : METRIC_FILTER_KEYS.ADJACENCIES_COMPARISON,
   },
+  adjacenciesUnit: {
+    labelKey       : 'AdjacenciesUnitFilter__label',
+    translationKeys: [
+      'AdjacenciesUnitFilter__hours',
+      'AdjacenciesUnitFilter__percentages'
+    ],
+    filterKey      : METRIC_FILTER_KEYS.ADJACENCIES_UNIT,
+  }
 };
 
-debugger;
-
 const AdjacenciesComparisonFilter = createFilterComponent(filterConfigs.adjacenciesComparison);
+const AdjacenciesUnitFilter = createFilterComponent(filterConfigs.adjacenciesUnit);
+
+const AdjacenciesFilters = () => (
+  <Fragment>
+    <AdjacenciesComparisonFilter />
+    <AdjacenciesUnitFilter />
+  </Fragment>
+);
 
 const FilterRoutes = () => {
   return (
     <Switch>
-      {/* <Route component={() => null}/> */}
-      <Route path={RouterPaths.collaboration__adjacencies} component={AdjacenciesComparisonFilter}/>
+      <Route path={RouterPaths.collaboration__adjacencies} component={AdjacenciesFilters}/>
       {/* NOTE: add other filters here */}
     </Switch>
   );
