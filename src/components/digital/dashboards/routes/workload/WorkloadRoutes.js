ee436a7c00847d01148b0c71d1246d5922a204e7
Matthew Condit <matthew@humanyze.com>
2019-01-24 14:56:56 -0500

    remove unused import

--- src/components/digital/dashboards/routes/workload/WorkloadRoutes.js
+++ src/components/digital/dashboards/routes/workload/WorkloadRoutes.js
@@ -4,8 +4,6 @@ import { Route, Switch } from 'react-router-dom';
 import { RedirectWithSearch } from 'Src/components/common/link-with-search/LinkWithSearch';
 import DigitalAllocation
   from 'Src/components/common/data-vis-components/digital-allocation/DigitalAllocation';
-import DigitalResponseTime
-    from 'Common/data-vis-components/digital-response-time/DigitalResponseTime';
 import AverageWorkdayLength from 'Common/data-vis-components/average-workday-length/AverageWorkdayLength';
 
 const WorkloadRoutes = () => {
