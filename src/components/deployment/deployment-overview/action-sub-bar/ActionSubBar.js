fbc253fca63efc98b33cb715e30d3ec024f50d22
Jake Kaplan <jakekaplan@Jakes-MacBook-Pro.local>
2019-03-21 11:26:39 -0400

    ELEMENTS-2345: add sync-button

--- src/components/deployment/deployment-overview/action-sub-bar/ActionSubBar.js
+++ src/components/deployment/deployment-overview/action-sub-bar/ActionSubBar.js
@@ -7,6 +7,7 @@ import RouterPaths from 'RouterPaths';
 import * as R from 'ramda';
 import FileDownload from 'js-file-download';
 import classNames from 'classnames';
+import delay from 'Utils/delay';
 
 import './action-sub-bar.scss';
 
@@ -44,15 +45,56 @@ const onImportClicked = ({ hasDeploymentStartDate, openImportDialog, showImportS
     hasDeploymentStartDate ? openImportDialog() : showImportStartDateError();
 };
 
+const onSyncClicked = ({ deploymentId, bearerToken, isSyncing, setIsSyncing, setSyncComplete, showSyncError }) => async (e) => {
+    if (isSyncing) {
+       return;
+    }
+    setIsSyncing(true);
+
+    try {
+        const res = await AxiosRequestService.datasets.syncParticipantDataset(deploymentId, bearerToken);
+        const requestUUID = res.data.task.uuid;
+        let hasResolved = false;
+
+        while (!hasResolved && requestUUID) {
+            const res = await AxiosRequestService.datasets.pollParticpantSyncStatus(deploymentId, requestUUID, bearerToken);
+            console.log('Monitoring Sync: ', res.data.state);
+
+            if (res.data.state === 'SUCCESS') {
+                console.log('Sync completed completed with status: ', res.data.state);
+                hasResolved = true;
+            }
+            else if (res.data.state === 'FAILURE' || res.data.state === 'REVOKED') {
+                console.log('Sync errored with status: ', res.data.state);
+                hasResolved = true;
+                throw Error(res.data.result);
+            } else {
+                console.log(requestUUID);
+                await delay(1000);
+            }
+        }
+        setSyncComplete(true);
+        await delay(10000);
+    } catch (e) {
+        console.error(e);
+        showSyncError();
+    }
+    setIsSyncing(false);
+    setSyncComplete(false);
+};
+
 const enhance = compose(
     withState('isExporting', 'setIsExporting', false),
+    withState('isSyncing', 'setIsSyncing', false),
+    withState('syncComplete', 'setSyncComplete', false),
     withHandlers({
         onImportClicked,
-        onExportClicked
+        onExportClicked,
+        onSyncClicked
     }),
 );
 
-export const ActionSubBarPure = ({ onImportClicked, onExportClicked, isExporting, deploymentName, translations }) => {
+export const ActionSubBarPure = ({ onImportClicked, onExportClicked, onSyncClicked, isExporting, isSyncing, syncComplete, deploymentName, translations }) => {
     return (
         <div className='ActionSubBar'>
             <div className='ActionSubBar__section ActionSubBar__section-left'>
@@ -71,8 +113,12 @@ export const ActionSubBarPure = ({ onImportClicked, onExportClicked, isExporting
                     {translations.actionSubBar__import}
                 </div>
                 <div onClick={onExportClicked}
-                     className={classNames('ActionSubBar__text', { 'ActionSubBar__text--exporting': isExporting })}>
+                    className={classNames('ActionSubBar__text', { 'ActionSubBar__text--exporting': isExporting })}>
                     {isExporting ? translations.actionSubBar__exporting : translations.actionSubBar__export}
+                </div>
+                <div onClick={onSyncClicked}
+                    className={classNames('ActionSubBar__text', { 'ActionSubBar__text--syncing': isSyncing })}>
+                    {isSyncing ? (syncComplete ? translations.actionSubBar__syncComplete : translations.actionSubBar__syncing ) : translations.actionSubBar__sync}
                 </div>
             </div>
         </div>
@@ -90,7 +136,8 @@ const ActionSubBar = connect(
     (dispatch) => ({
         openImportDialog        : () => dispatch(openModal(MODAL_CONFIGS.importParticipantsConfig)),
         showImportStartDateError: () => dispatch(addFlashErrorWithFadeout(ErrorMessages.noImportStartDateError)),
-        showExportError         : () => dispatch(addFlashErrorWithFadeout(ErrorMessages.participantExportFailure))
+        showExportError         : () => dispatch(addFlashErrorWithFadeout(ErrorMessages.participantExportFailure)),
+        showSyncError           : () => dispatch(addFlashErrorWithFadeout(ErrorMessages.participantSyncFailure))
 
     })
 )(enhance(ActionSubBarPure));
